2c53c59a082a47478dd01ddc19bcb5c8d3646b66
miriamtruiolo <miriamtruiolo@gmail.com>
2024-10-12 16:12:13 +0000

    fix bug send email

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,16 @@
-function sendMail() {
-    let params = {
-       user_name : document.getElementById("user_name").value,
-       user_email : document.getElementById("user_email").value,
-       user_message : document.getElementById("message").value
-    }
+function sendMail(event) {
+  event.preventDefault(); // Aggiungi questo per prevenire il submit del form
 
-    emailjs.send("service_m14b9nb","template_7weeg6q",params)
-    .then(function (res) {
-        alert("Your data has been successfully sent! ");
-      }, function(err) {
-        alert("Something went wrong: ", err);
-      });
+  let params = {
+     user_name: document.getElementById("user_name").value,
+     user_email: document.getElementById("user_email").value,
+     user_message: document.getElementById("message").value
+  }
+
+  emailjs.send("service_m14b9nb", "template_7weeg6q", params)
+  .then(function (res) {
+      alert("Your data has been successfully sent! ");
+  }, function (err) {
+      alert("Something went wrong: " + JSON.stringify(err));
+  });
 }
