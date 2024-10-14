2dbcddfc3eae764a1777dc2c9900687b0fb55ad7
miriamtruiolo <miriamtruiolo@gmail.com>
2024-10-14 20:17:24 +0200

    requested changes made

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,24 @@
 function sendMail(event) {
-  event.preventDefault(); // Aggiungi questo per prevenire il submit del form
+  event.preventDefault();
 
   let params = {
-     user_name: document.getElementById("user_name").value,
-     user_email: document.getElementById("user_email").value,
-     user_message: document.getElementById("message").value
+    user_name: document.getElementById("user_name").value,
+    user_email: document.getElementById("user_email").value,
+    user_message: document.getElementById("message").value
   }
 
   emailjs.send("service_m14b9nb", "template_7weeg6q", params)
-  .then(function (res) {
+    .then(function (res) {
       alert("Your data has been successfully sent! ");
-  }, function (err) {
+    }, function (err) {
       alert("Something went wrong: " + JSON.stringify(err));
-  });
+    });
 }
+
+const form = document.querySelector('form');
+const submitBtn = document.getElementById('submit-btn');
+
+form.addEventListener('input', () => {
+  const isValid = [...form.elements].every(input => input.checkValidity());
+  submitBtn.disabled = !isValid;
+});
