a60668d3f0dc3766124326b97ec748557e3794ee
miriamtruiolo <miriamtruiolo@gmail.com>
2024-10-15 18:59:40 +0200

    add refactor

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,6 +10,7 @@ function sendMail(event) {
   emailjs.send("service_m14b9nb", "template_7weeg6q", params)
     .then(function (res) {
       alert("Your data has been successfully sent! ");
+      form.reset();
     }, function (err) {
       alert("Something went wrong: " + JSON.stringify(err));
     });
